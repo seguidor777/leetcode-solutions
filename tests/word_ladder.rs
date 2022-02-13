b7a227116006782c6073ba342397181f48ad4532
Jorge Luna <seguidor777@gmail.com>
2022-02-13 13:48:54 -0700

    add leetcode_prelude

--- tests/word_ladder.rs
+++ tests/word_ladder.rs
@@ -1,15 +1,24 @@
+use leetcode_prelude::vec_string;
 use leetcode_solutions::word_ladder;
 
 #[test]
-fn word_ladder_test() {
+fn word_ladder_test_case_1() {
     let res = word_ladder::Solution::ladder_length(
         "hit".to_string(),
         "cog".to_string(),
-        vec!["hot", "dot", "dog", "lot", "log", "cog"]
-            .iter()
-            .map(|&x| x.into())
-            .collect(),
+        vec_string!["hot", "dot", "dog", "lot", "log", "cog"],
     );
 
     assert_eq!(5, res);
+}
+
+#[test]
+fn word_ladder_test_case_2() {
+    let res = word_ladder::Solution::ladder_length(
+        "hit".to_string(),
+        "cog".to_string(),
+        vec_string!["hot", "dot", "dog", "lot", "log"],
+    );
+
+    assert_eq!(0, res);
 }
